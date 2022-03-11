36f2f54752b8d9178f4a2c0c67c2b300b3785b00
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-03-11 23:49:35 +0900

    read

--- read_data.py
+++ read_data.py
@@ -8,7 +8,14 @@ def read_csv_data(file_path):
     Returns:
         list: data split row
     """
+    
     result_data = []
+
     # WRITE YOUR CODE HERE
+    result_data=file_path.split('\n')
+    
     return result_data
+file_path=open('data\weight-height.csv').read()
+print(read_csv_data(file_path))
+
     
