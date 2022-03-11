cd27b79f8f93f3d914f17724a15451816d902d6d
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-03-12 01:37:58 +0900

    get_data

--- get_data.py
+++ get_data.py
@@ -12,10 +12,56 @@ def get_data(data):
         tuple: gender, weight and height
 
     """
+    old_gender=[]
+    pound_weight=[]
+    inch_height=[]
     gender = []
     weight = []
     height = []
     
     # WRITE YOUR CODE HERE
 
+    #-----------------------------Gender ------------------------------------------
+
+    coloumn=file_path.split('\n')
+    for idx in range(len(coloumn)):
+        coloumn2=coloumn[idx].split(',')
+        a=coloumn2[0]
+        old_gender.append(a)
+    del old_gender[0]
+    
+    for i in range(len(old_gender)):
+        if old_gender[i]=='"Male"':
+            gender.append(0)
+        else:
+            gender.append(1)
+    #-----------------------------Weight ------------------------------------------
+
+    for idx in range(len(coloumn)):
+        coloumn2=coloumn[idx].split(',')
+        b=coloumn2[2]
+        pound_weight.append(b)
+    del pound_weight[0]
+    
+    for idx2 in range(len(pound_weight)):
+        kg=float(pound_weight[idx2])/2.205
+        weight.append(kg)
+    
+    #-----------------------------Height ------------------------------------------
+
+    for idx in range(len(coloumn)):
+        coloumn2=coloumn[idx].split(',')
+        c=coloumn2[1]
+        inch_height.append(c)
+    del inch_height[0]
+    
+    for idx3 in range(len(inch_height)):
+        cm=float(pound_weight[idx3])*2.54
+        height.append(cm)
+
+
+    
     return gender,weight,height
+file_path=open('data/weight-height.csv').read()
+data=read_csv_data(file_path)
+print(get_data(data))
